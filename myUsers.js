fb5db375f02c90324525f6553392b1877b7f09b3
zhouyca <zhouyca@yonyou.com>
2016-09-22 16:45:54 +0800

    增加本地字体图标文件

--- myUsers.js
+++ myUsers.js
@@ -4,39 +4,33 @@ var appModule = angular.module('scotchApp', ['ngRoute']);
 appModule.config(function($routeProvider) {
 	$routeProvider
 	// route for the home page
-        .when('/home', {
+        .when('/', {
             templateUrl : 'pages/home.html',
             controller  : 'mainController'
         })
 
-        // route for the about page
+        // route for the tabs page
         .when('/tabs', {
         	templateUrl : 'pages/tabs.html',
-        	controller  : 'aboutController'
+        	controller  : 'tabsController'
         })
 
-        // route for the contact page
+        // route for the buttons page
         .when('/buttons', {
             templateUrl : 'pages/buttons.html',
-            controller  : 'contactController'
+            controller  : 'buttonsController'
         })
-
-        .otherwise('/home');
+		// route for the stars page
+        .when('/stars', {
+            templateUrl : 'pages/stars.html',
+            controller  : 'starsController'
+        })
+        .otherwise('/');
 });
 
 var mainCtrl = function($scope){
 	$scope.message = 'Everyone come and see how good I look!';
 }
 
-var aboutCtrl = function($scope){
-	$scope.message = 'Look! I am an about page.';
-}
-
-var contactCtrl = function($scope){
-	$scope.message = 'Contact us! JK. This is just a demo.';
-}
-
-
 appModule.controller('mainController', mainCtrl);
-appModule.controller('aboutController', aboutCtrl);
-appModule.controller('contactController', contactCtrl);
+
--- public/js/pages.js
+++ public/js/pages.js
@@ -3,13 +3,34 @@ $('.silder li').on('click', function() {
 	$(this).addClass('active').siblings().removeClass('active');
 })
 //tab切换效果
-//$('.tabs').each(function() {
-//	var i = $(this).index();
-//	$(this).find('.tab').addClass("tab_" + i).find('li').on('click', function() {
-//		$(this).addClass('active').siblings().removeClass('active');
-//
-//		$(this).parents('.tab').siblings('.tab-content').children('div').eq($(this).index()).show().siblings().hide();
-//	});
-//
-//});
+$('.tabs').each(function() {
+	var i = $(this).index();
+	$(this).find('.tab').addClass("tab_" + i).find('li').on('click', function() {
+		$(this).addClass('active').siblings().removeClass('active');
 
+		$(this).parents('.tab').siblings('.tab-content').children('div').eq($(this).index()).show().siblings().hide();
+	});
+
+});
+
+//$('.tabs').each(function(){
+//	$(this).find('tab').each(function(){
+//		$(this).find('li').on('click',function({
+//			$(this).addClass('active').siblings().removeClass('active');
+//			$('.tab').siblings('.tab-content').children('div').eq($(this).index()).addClass('active').siblings().removeClass('active');
+//		}))
+//	})
+//});
+//
+//appModule.controller('tabsController', function($scope) {
+//	
+//	
+//  $('.tabs').each(function(){
+//			$(this).find('tab').each(function(){
+//				$(this).find('li').on('click',function({
+//					$(this).addClass('active').siblings().removeClass('active');
+//					$('.tab').siblings('.tab-content').children('div').eq($(this).index()).addClass('active').siblings().removeClass('active');
+//				}))
+//			})
+//		});
+//});
